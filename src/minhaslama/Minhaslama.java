be170dea062334d8b80a705190e2c45f29b87d70
Rubens Bitencourt <contato@rubensconsult.com.br>
2026-06-15 21:10:51 -0300

    Um é igual a Um

---
+++ src/minhaslama/Minhaslama.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package minhaslama;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Minhaslama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        if(1==1){
+        System.out.println("Um é igual a um");// TODO code application logic here
+    }
+    
+}
+    
+}
